4867d17e51a67a62bdd9117077f4b254019eddc6
Aka2210 <qoo0973215128@gmail.com>
2023-07-05 19:58:24 +0800

    Finish Player (important)

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -8,7 +8,7 @@ public class AnimationController : MonoBehaviour
 
     float velocityX = 0f, velocityZ = 0f;
     public float acceleration = 1f;
-    float jump = 0f;
+    float jump = 0f, fall = 0f;
 
     void Start()
     {
@@ -17,22 +17,22 @@ public class AnimationController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey("w") && velocityX < 1f) //若按下w且動畫尚未完全撥放，則持續增加velocityX使動畫完全撥放
+        if(Input.GetKey("w") && velocityX < 1f && GetComponent<PlayerInformation>().isGrounded) //若按下w且動畫尚未完全撥放，則持續增加velocityX使動畫完全撥放
         {
             velocityX += acceleration * Time.deltaTime;
         }
 
-        if(Input.GetKey("s") && velocityX > -1f) //若按下s且動畫尚未完全撥放，則持續減少velocityX使動畫完全撥放
+        if(Input.GetKey("s") && velocityX > -1f && GetComponent<PlayerInformation>().isGrounded) //若按下s且動畫尚未完全撥放，則持續減少velocityX使動畫完全撥放
         {
             velocityX -= acceleration * Time.deltaTime;
         }
 
-        if(Input.GetKey("a") && velocityZ > -1f) //同上述理論
+        if(Input.GetKey("a") && velocityZ > -1f && GetComponent<PlayerInformation>().isGrounded) //同上述理論
         {
             velocityZ -= acceleration * Time.deltaTime;
         }
 
-        if(Input.GetKey("d") && velocityZ < 1f) //同上述理論
+        if(Input.GetKey("d") && velocityZ < 1f && GetComponent<PlayerInformation>().isGrounded) //同上述理論
         {
             velocityZ += acceleration * Time.deltaTime;
         }
@@ -47,10 +47,14 @@ public class AnimationController : MonoBehaviour
             velocityX = 0f;
         }
 
-        if(Input.GetKey("left ctrl") && Input.GetKey("w") && GetComponent<PlayerMovement>().isGrounded) //若按下左ctrl則撥放奔跑動畫並增加移動速度。
+        if(Input.GetKey("left ctrl") && Input.GetKey("w") && GetComponent<PlayerInformation>().isGrounded && animator.GetFloat("Jumpping") ==0) //若按下左ctrl則撥放奔跑動畫並增加移動速度。
         {
-            velocityX = 2f;
-            GetComponent<PlayerMovement>().speed = 5f;
+            if(velocityX < 2f)
+                velocityX += acceleration * Time.deltaTime;
+            else
+                velocityX = 2f;
+
+            GetComponent<PlayerMovement>().CurrentSpeed = GetComponent<PlayerInformation>().PlayerRunningSpeed;
         }
         else if(Input.GetKeyUp("left ctrl")) //若無按壓左ctrl則瞬間停止動畫(以此達到放開左ctrl即停止移動的視覺效果)
         {
@@ -58,24 +62,34 @@ public class AnimationController : MonoBehaviour
             {
                 velocityX = 1f;
             }
-            GetComponent<PlayerMovement>().speed = 3f;
+            GetComponent<PlayerMovement>().CurrentSpeed = GetComponent<PlayerInformation>().PlayerWalkingSpeed;
         }
 
-        if(GetComponent<PlayerMovement>().Jumping)
+        if(GetComponent<PlayerInformation>().PlayerJumpping)
         {
             velocityX = 0;
             velocityZ = 0;
             jump = 1;
         }
-        else if(!GetComponent<PlayerMovement>().isGrounded)
+        else if(!GetComponent<PlayerInformation>().isGrounded)
         {
-            jump -= acceleration / GetComponent<PlayerMovement>().maxTime;
+            fall += acceleration / GetComponent<PlayerMovement>().maxTime;
         }
         else
         {
-            jump = 0;
+            // jump = 0;
+            if(jump > 0)
+                jump -= acceleration / GetComponent<PlayerMovement>().maxTime;
+            else
+                jump = 0;
+            
+            if(fall > 0)
+                fall -= acceleration / GetComponent<PlayerMovement>().maxTime;
+            else
+                fall = 0;
         }
 
+        animator.SetFloat("Falling", fall);
         animator.SetFloat("Jumpping", jump);
         animator.SetFloat("Velocity X", velocityX);
         animator.SetFloat("Velocity Z", velocityZ);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
- 
-    public float speed = 3f;
-    public float gravity = 9.81f;
-    public float jumpHeight = 10f;
- 
-    public Transform groundCheck, ceilingCheck;
-    public float groundDistance = 0.4f, ceilingDistance = 0.2f;
-    public LayerMask groundMask, ceilingMask;
+
     float time = 0f;
-    public float FPS = 60f;
-    public bool Jumping = false;
-    public float deltaTime, maxTime;
+    public float deltaTime, maxTime, CurrentSpeed;
  
     Vector3 velocity;
 
-    public bool isCeiling;
-    public bool isGrounded;
-    
+    float FPS, JumpHeight, Gravity;
+
     void Start() 
     {
+        FPS = GetComponent<PlayerInformation>().GameFPS;
+        JumpHeight = GetComponent<PlayerInformation>().PlayerJumpHeight;
+        Gravity = GetComponent<PlayerInformation>().EnvirnomentGravity;
+        CurrentSpeed = GetComponent<PlayerInformation>().PlayerWalkingSpeed;
+
         QualitySettings.vSyncCount = 0;   // 把垂直同步關掉
         Application.targetFrameRate = (int)FPS; //設定畫面幀數
-        deltaTime = Mathf.Sqrt(2f * jumpHeight / gravity); //根據跳躍高度、重力計算此跳躍需要t秒
-        maxTime = FPS * deltaTime; //等比例計算t秒代表要跑幾次Update()函式    
+        deltaTime = Mathf.Sqrt(2f * JumpHeight / Gravity); //根據跳躍高度、重力計算此跳躍需要t秒
+        maxTime = FPS * deltaTime; //等比例計算t秒代表要跑幾次Update()函式
     }
 
     void Update()
     {
-        //Physics.CheckSphere()函式用以偵測給定位置周圍是否有碰撞器。 重點:groundMask負責檢測哪一個層級(Layer)的碰撞器才會被偵測。
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        GetComponent<PlayerInformation>().DetectGround();
 
         //抓取水平和垂直軸的移動比例(-1 <= x,z <= 1)
         float x = Input.GetAxis("Horizontal");
@@ -44,38 +38,38 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         //利用CharacterController的內建函式來處理角色移動(位移比例*移動速度*幀數修正)。
-        controller.Move(move * speed * 1 / FPS);
+        controller.Move(move * CurrentSpeed * 1 / FPS);
         
         //偵測是否在地面按下空白鍵(跳躍鍵)
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<PlayerInformation>().isGrounded)
         {
             time = (float)((int)maxTime + 1); //設定當前time值(用來計算跑幾次Update後結束跳躍)
-            Jumping = true; //跳躍開始
+            GetComponent<PlayerInformation>().PlayerJumpping = true; //跳躍開始
         }
 
-        if(Jumping)
+        if(GetComponent<PlayerInformation>().PlayerJumpping)
         {
-            velocity.y = (gravity * deltaTime - (maxTime - time) * gravity / maxTime) / maxTime - 1f / 2f * gravity / maxTime / maxTime; //=號後方代表向上移動的距離
+            velocity.y = (Gravity * deltaTime - (maxTime - time) * Gravity / maxTime) / maxTime - 1f / 2f * Gravity / maxTime / maxTime; //=號後方代表向上移動的距離
             controller.Move(velocity); //velocity代表每一次update使玩家移動此距離
             time--;
 
-            if(!isCeiling) //偵測是否頂到天花板
-                isCeiling = Physics.CheckSphere(ceilingCheck.position, ceilingDistance, ceilingMask);
-            else if(isGrounded) //若頂到天花板則只有再次觸碰地面會使跳躍重置
-                isCeiling = Physics.CheckSphere(ceilingCheck.position, ceilingDistance, ceilingMask);
+            if(!GetComponent<PlayerInformation>().isCeiling) //偵測是否頂到天花板
+                GetComponent<PlayerInformation>().DetectCeiling();
+            else if(GetComponent<PlayerInformation>().isGrounded) //若頂到天花板則只有再次觸碰地面會使跳躍重置
+                GetComponent<PlayerInformation>().DetectCeiling();
 
-            if(time == 0 || isCeiling)
+            if(time == 0 || GetComponent<PlayerInformation>().isCeiling)
             {
-                Jumping = false;
+                GetComponent<PlayerInformation>().PlayerJumpping = false;
             }
         }
-        else if(!Jumping)
+        else if(!GetComponent<PlayerInformation>().PlayerJumpping)
         {
-            velocity.y = ((time + 1f/2f) * gravity) / (maxTime * maxTime); //每一次update使玩家移動此距離
+            velocity.y = ((time + 1f/2f) * Gravity) / (maxTime * maxTime); //每一次update使玩家移動此距離
             controller.Move(-velocity);
             time++;
 
-            if(isGrounded) //如果已經接觸地面則重置當前的time，否則持續掉落
+            if(GetComponent<PlayerInformation>().isGrounded) //如果已經接觸地面則重置當前的time，否則持續掉落
             {
                 time = 0;
             }
